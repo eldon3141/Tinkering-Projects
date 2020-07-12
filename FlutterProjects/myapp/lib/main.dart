bef983761dedc450babc35dd820dcf3759b99136
Eldon Ngo <40747060+eldon3141@users.noreply.github.com>
2020-07-12 01:02:45 -0700

    Implemented basic recommendation

--- FlutterProjects/myapp/lib/main.dart
+++ FlutterProjects/myapp/lib/main.dart
@@ -3,53 +3,105 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-          ], //Children of Column
-        )),
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('4.1'), //Rating
-      ], //Children
-    ),
-  );
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  'A Paper Store',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'El Monte, California',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ], //Children of Column
+          )),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('4.1'), //Rating
+        ], //Children
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        "Come on out for some tasty paper!",
+        softWrap: true,
+      ),
+    );
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Sample App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Paper Eatery'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/paperstore-featured.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
     );
-  }
-}
+  } //End of build Widget
+
+  //Build the button columns
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ], //End of Children
+    ); //End of Column
+  } //End of buildButtonColumn
+
+} //End of Stateless Widget
